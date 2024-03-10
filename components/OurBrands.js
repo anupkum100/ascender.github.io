bcaa56b6df45e3a86f7edc3747d81e677d95603c
Anup Kumar <anupkumar@Anups-MacBook-Pro.local>
2024-03-10 19:43:18 +0530

    Added sliding on brands

--- components/OurBrands.js
+++ components/OurBrands.js
@@ -9,10 +9,18 @@ function OurBrands() {
     }
     return <section className="ascender-light p-5">
         <h2 className="text-bold text-center">Brands trusted on us</h2>
-        <div className="row">
+        {/* <div className="row">
             {brands.map(brand => <div key={brand} className="p-3 col-2">
                 <img src={brand} />
             </div>)}
+        </div> */}
+
+        <div className="slider-container">
+            <div className="slider">
+                <div className="logos">
+                    {brands.map(brand => <img className="me-5" src={brand} />)}
+                </div>
+            </div>
         </div>
     </section>
 }
